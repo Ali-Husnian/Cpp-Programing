5fd4f236b63e5b8334bd2bbb0ab14a87092c46f5
Ali Husnain <pkihusnain220@gmail.com>
2022-09-05 00:19:56 -0700

    modify add another program

--- if-else.cpp
+++ if-else.cpp
@@ -2,19 +2,47 @@
 using namespace std ;
 
 main(){
-    int number ;
+    /*
+        int number ;
     cout << "Enter the Whole Number : ";
     cin >> number;
 
-if(number % 2 == 0)
-{
-    cout << "You have entered even number ";
-}
-else
-{
-        cout << "You have entered odd number ";
+    if(number % 2 == 0)
+    {
+        cout << "You have entered even number ";
+    }
+    else
+    {
+            cout << "You have entered odd number \n";
 
-}
+    }
+
+    */
+    //user enter side lengths of a triangle (a,b,c)
+    //program should write out wheter triangle is
+    //equilateral . isosceles or scalene
+    
+    float a, b, c;
+    cout << " Ente the Value a , b a , c :" ;
+    cin >> a >> b >> c;
+
+    if(a==b && b==c && a==c)
+    {
+        cout << "Equilateral triangle :";
+    }
+    else
+    {
+        if(a!=b && b!=c && a!=c)
+        {
+            cout << "Isosceles triangle :";
+
+        }
+        else
+        {
+            cout << "Sceles triangle :";
+
+        }
+    }
 
     system("pause>0");
 
