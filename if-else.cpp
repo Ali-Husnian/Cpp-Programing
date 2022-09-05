780668694f1b384914561db8cccf9ede740a70f9
Ali Husnain <pkihusnain220@gmail.com>
2022-09-04 23:20:01 -0700

    add a new if-else.cpp file

---
+++ if-else.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std ;
+
+main(){
+    int number ;
+    cout << "Enter the Whole Number : ";
+    cin >> number;
+
+if(number % 2 == 0)
+{
+    cout << "You have entered even number ";
+}
+else
+{
+        cout << "You have entered odd number ";
+
+}
+
+    system("pause>0");
+
+}
