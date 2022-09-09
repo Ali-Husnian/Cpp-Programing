3d15126e350b1b647e9eed5a39227ea465e00c25
Ali Husnain <pkihusnain220@gmail.com>
2022-09-10 01:39:53 +0500

    new file program for swapping value of tow variable

---
+++ swapping-value.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std ;
+
+ main()
+{
+    //program for swapping value of tow variable
+
+    int a = 40 ;
+    int b = 20 ;
+    int temp = a ;
+    a = b;
+    b = temp;
+    cout << "\n Value of b = " << b ;
+    cout << "\n Value of a = " << a ;
+
+    //program for swapping value of tow variable mathemathical
+        int p =20;
+        int q =10;
+        p = p+q;
+        q = p-q ;
+        p = p- q;
+        cout << "\n Value of p = "<< p ;
+        cout << "\n Value of q = "<< q ;
+    return 0;
+
+}
